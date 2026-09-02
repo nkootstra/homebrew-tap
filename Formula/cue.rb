c35308cc78e3d19160b4f08ad538c806946171b9
Niels Kootstra <545768+nkootstra@users.noreply.github.com>
2026-09-02 07:06:30 +0000

    cue 0.15.1

--- Formula/cue.rb
+++ Formula/cue.rb
@@ -1,26 +1,26 @@
 class Cue < Formula
   desc "Turn video and audio files into transcripts, subtitles, and descriptions"
   homepage "https://github.com/nkootstra/cue"
-  version "0.15.0"
+  version "0.15.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/nkootstra/cue/releases/download/v0.15.0/cue-aarch64-apple-darwin.tar.gz"
-      sha256 "d85173b516834055664bd3ef95f89b1f24e5b0aee8d9de594b97169eaae7dfa9"
+      url "https://github.com/nkootstra/cue/releases/download/v0.15.1/cue-aarch64-apple-darwin.tar.gz"
+      sha256 "1622d442166ccb19bb9908e9dd14be6a4ae32bcd431cf9cfc1eacebe15e2248b"
     else
-      url "https://github.com/nkootstra/cue/releases/download/v0.15.0/cue-x86_64-apple-darwin.tar.gz"
-      sha256 "d9dd9c58eebfa9ce4399d6159a99ea60770b2cf6d39fd1be35095af624f19e8f"
+      url "https://github.com/nkootstra/cue/releases/download/v0.15.1/cue-x86_64-apple-darwin.tar.gz"
+      sha256 "6d717ad62fa154d39dbd7a882189727b0a1e744261c258a484eb23613ba7c2bd"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/nkootstra/cue/releases/download/v0.15.0/cue-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "4f92907528b7a82edb319bfebf9647b0067a16872b6e12f7bfb5731e0ddb9d78"
+      url "https://github.com/nkootstra/cue/releases/download/v0.15.1/cue-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "d13e3e2cce1407000f1b42c07a811fa818dd4e159dd28024f9d6eb6964ab0a5f"
     else
-      url "https://github.com/nkootstra/cue/releases/download/v0.15.0/cue-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "575020006d8b8ab495cd595eeffef920013d58eb166cd09723555298bb579105"
+      url "https://github.com/nkootstra/cue/releases/download/v0.15.1/cue-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "699f9dbb338edd39c8aec456c625cc78d176b6b35ad2118dd9669de7cd68d4f1"
     end
   end
 
@@ -39,6 +39,6 @@ class Cue < Formula
   end
 
   test do
-    assert_equal "cue #0.15.0", shell_output("#{bin}/cue --version").strip
+    assert_equal "cue 0.15.1", shell_output("#{bin}/cue --version").strip
   end
 end
